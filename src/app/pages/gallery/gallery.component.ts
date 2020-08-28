080912807268b2f421f6b067d7fcb62bfeb2ae3c
Sara Casasola <scasasolaborrego@sequel.com>
2020-08-28 16:26:29 +0100

    Galeria de fotos

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -14,11 +14,15 @@ export class GalleryComponent implements OnInit {
   private smallerFactor = 0.95;
   private playerInterval = 2000;
   private playerTimer = timer(0, this.playerInterval);
+  private carouselInterval = 3;
+  private lastCarouselItemsLength: number;
 
   public galleryContent: GalleryContent[] = [];
   public currentgalleryItem = 0;
   public isPlaying = false;
   public previewHeight = this.initialPreviewHeight;
+  public carouselStarts = 0;
+  public carouselFinish = this.carouselInterval;
 
   constructor(private authService: AuthService, private galleryService: GalleryService) {
     if (!this.authService.isLogged()) {
@@ -27,12 +31,14 @@ export class GalleryComponent implements OnInit {
     }
 
     this.galleryContent = this.galleryService.getGalleryContent();
+    this.lastCarouselItemsLength = this.galleryContent.length % this.carouselInterval;
   }
 
-  public goToContent(galleryItem: number) {
+  public goToContent(galleryItem: number): void {
+    if (this.isPlaying) { return; }
     this.currentgalleryItem = galleryItem;
   }
-  
+
   public isPreviusButtonDisabled(): boolean {
     return this.isFirstItem();
   }
@@ -90,6 +96,46 @@ export class GalleryComponent implements OnInit {
       this.currentgalleryItem = this.currentgalleryItem + 1;
 
     });
+  }
+
+  public onNextCarouselClick(): void {
+    const nextStartIndex = this.carouselStarts + this.carouselInterval;
+
+    if (nextStartIndex >= this.galleryContent.length) {
+      this.goToInitalCarouselSet();
+      return;
+    }
+
+    this.carouselStarts = nextStartIndex;
+    this.carouselFinish = nextStartIndex + this.carouselInterval;
+  }
+
+  private goToInitalCarouselSet(): void {
+    this.carouselStarts = 0;
+    this.carouselFinish = this.carouselInterval;
+  }
+
+  public onPreviousCarouselClick(): void {
+    const previousStartIndex = this.carouselStarts - this.carouselInterval;
+
+    if (previousStartIndex < 0 ) {
+      this.goToLastCarouselSet();
+      return;
+    }
+
+    this.carouselStarts = previousStartIndex;
+    this.carouselFinish = previousStartIndex + this.carouselInterval;
+  }
+
+  private goToLastCarouselSet(): void {
+    if (this.lastCarouselItemsLength === 0) {
+      this.carouselStarts = this.galleryContent.length - this.carouselInterval;
+      this.carouselFinish = this.carouselStarts + this.carouselInterval;
+      return;
+    }
+
+    this.carouselStarts = this.galleryContent.length - this.lastCarouselItemsLength;
+    this.carouselFinish = this.carouselStarts + this.lastCarouselItemsLength;
   }
 
   public ngOnInit(): void {
